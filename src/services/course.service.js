b3d3c213ee600fecdb8b58afb475b93422da2e42
Jogle Chang <jogle chang@gmail.com>
2023-07-14 00:56:50 +0800

    API

--- src/services/course.service.js
+++ src/services/course.service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:8080/api/courses";
+const API_URL = "https://mern-server-urejhlf77a-de.a.run.app/api/courses";
 
 class CourseService {
     post(title, description, price) {
