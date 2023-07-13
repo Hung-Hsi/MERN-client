dd034011af47312d38aeac47759c25a18c3a1b2e
Jogle Chang <jogle chang@gmail.com>
2023-07-13 19:58:24 +0800

    push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import NavComponent from "./components/nav-component";
+import HomeComponent from "./components/home-component";
+import RegisterComponent from "./components/register-component";
+import LoginComponent from "./components/login-component";
+import ProfileComponent from "./components/profile-component";
+import CourseComponent from "./components/course-component";
+import PostCourseComponent from "./components/postCourse-component";
+import EditCourseComponent from "./components/editCourse-component";
+import EnrollComponent from "./components/enroll-component";
+import AuthService from "./services/auth.service";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [currentUser, setCurrentUser] = useState(AuthService.getCurrentUser());
+    const [patchCourseId, setPatchCourseId] = useState("");
 
+    return (
+        <div>
+            <NavComponent
+                currentUser={currentUser}
+                setCurrentUser={setCurrentUser}
+            />
+            <Routes>
+                <Route path="/" element={<HomeComponent />} />
+                <Route path="/register" element={<RegisterComponent />} />
+                <Route path="/login" element={
+                    <LoginComponent
+                        currentUser={currentUser}
+                        setCurrentUser={setCurrentUser}
+                    />
+                }/>
+                <Route path="/profile" element={
+                    <ProfileComponent
+                        currentUser={currentUser}
+                        setCurrentUser={setCurrentUser}
+                    />
+                }/>
+                <Route path="/course" element={
+                    <CourseComponent
+                        currentUser={currentUser}
+                        setCurrentUser={setCurrentUser}
+                        patchCourseId={patchCourseId}
+                        setPatchCourseId={setPatchCourseId}
+                    />
+                }/>
+                <Route path="/postCourse" element={
+                    <PostCourseComponent
+                        currentUser={currentUser}
+                        setCurrentUser={setCurrentUser}
+                    />
+                }/>
+                <Route path="/editCourse" element={
+                    <EditCourseComponent
+                        currentUser={currentUser}
+                        setCurrentUser={setCurrentUser}
+                        patchCourseId={patchCourseId}
+                        setPatchCourseId={setPatchCourseId}
+                    />
+                }/>
+                <Route path="/enroll" element={
+                    <EnrollComponent
+                        currentUser={currentUser}
+                        setCurrentUser={setCurrentUser}
+                    />
+                }/>
+            </Routes>
+        </div>
+    )
+}
 export default App;
---
+++ src/components/enroll-component.js
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import CourseService from '../services/course.service';
+
+const EnrollComponent = (props) => {
+    let { currentUser, setCurrentUser } = props;
+    const navigate = useNavigate();
+    let [searchInput, setSearchInput] = useState("");
+    let [searchResult, setSearchResult] = useState([]);
+
+    const handleTakeToLogin = () => {
+        navigate("/login");
+    };
+    const handleChangeInput = (e) => {
+        setSearchInput(e.target.value);
+    };
+    const handleSearch = () => {
+        if (searchInput.trim() !== "") { // 去除字符串開頭和結尾的空白字符後 不為空值
+            CourseService.getCourseByName(searchInput.trim())
+                .then((data) => {
+                    setSearchResult(data.data);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        }
+    };
+    const handleEnroll = (e) => {
+        // console.log(e.target);
+        CourseService.enroll(e.target.id, currentUser.user._id)
+            .then(() => {
+                window.alert("Done Enrollment");
+                navigate("/course");
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
+    useEffect(() => {
+        console.log("Using effect.");
+        if (searchInput.trim() == "") { 
+            CourseService.getAll()
+                .then((data) => {
+                    setSearchResult(data.data);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        }
+    }, [searchInput.trim() == ""]);
+
+    return (
+        <div style={{ padding: "3rem" }}>
+            {!currentUser && (
+                <div>
+                    <p>You must login first before searching for courses.</p>
+                    <button class="btn btn-primary btn-lg" onClick={handleTakeToLogin}>
+                        Take me to login page.
+                    </button>
+                </div>
+            )}
+            {currentUser && currentUser.user.role == "instructor" && (
+                <div>
+                    <h1>Only students can enroll in courses.</h1>
+                </div>
+            )}
+            {currentUser && currentUser.user.role == "student" && (
+                <div className="search input-group mb-3">
+                    <input
+                        onChange={handleChangeInput}
+                        type="text"
+                        class="form-control"
+                    />
+                    <button onClick={handleSearch} className="btn btn-primary">
+                        Search
+                    </button>
+                </div>
+            )}
+            {currentUser && searchResult && searchResult.length != 0 ? (
+                <div>
+                    <p>Data we got back from API.</p>
+                    {searchResult.map((course) => (
+                        <div key={course._id} className="card" style={{ width: "18rem" }}>
+                            <div className="card-body">
+                                <h5 className="card-title">{course.title}</h5>
+                                <p className="card-text">{course.description}</p>
+                                <p>Price: {course.price}</p>
+                                <p>Student: {course.students.length}</p>
+                                <a
+                                    href="#"
+                                    onClick={handleEnroll}
+                                    className="card-text btn btn-primary"
+                                    id={course._id}
+                                >
+                                    Enroll
+                                </a>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <p>No search results found.</p>
+            )}
+        </div>
+    );
+}
+
+export default EnrollComponent;
